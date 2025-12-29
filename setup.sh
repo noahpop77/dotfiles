14f8ab75a6578ff4d8d270c2d8b0701235f83ae3
noahpop77 <noahpop77@gmail.com>
2025-12-29 00:47:17 +0000

    Expanded script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
+set -e
+
 sudo cp gitupdate /usr/bin/
+echo "---gitupdate copied to /usr/bin/"
+
+PACKAGES=(
+  curl
+  git
+  neovim
+  htop
+  cmatrix
+)
+
+NEEDED=()
+
+for pkg in "${PACKAGES[@]}"; do
+    dpkg -s "$pkg" >/dev/null 2>&1 || NEEDED+=("$pkg")
+done
+
+if [ ${#NEEDED[@]} -gt 0 ]; then
+    sudo apt update
+    sudo apt install -y "${NEEDED[@]}"
+else
+    echo "---All packages already installed"
+fi
